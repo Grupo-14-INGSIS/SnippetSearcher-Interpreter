e9057cfba4fa96703c94a7b4ca562002705b4aeb
SergioMorero <smorero@mail.austral.edu.ar>
2025-09-30 22:18:42 -0300

    Initialized SpringBoot

---
+++ src/main/kotlin/com/Grupo14IngSis/SnippetSearcher_Runner/SnippetSearcherRunnerApplication.kt
@@ -0,0 +1,11 @@
+package com.Grupo14IngSis.SnippetSearcher_Runner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetSearcherRunnerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetSearcherRunnerApplication>(*args)
+}
---
+++ src/test/kotlin/com/Grupo14IngSis/SnippetSearcher_Runner/SnippetSearcherRunnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Grupo14IngSis.SnippetSearcher_Runner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetSearcherRunnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
